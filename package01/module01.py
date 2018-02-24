7d7ac5dbb720f43269f0e6d076b01d2fe4a6665c
lockhat <1160444584@qq.com>
2018-02-24 16:05:32 +0800

    本地新增了包package01和模块module01

---
+++ package01/module01.py
@@ -0,0 +1,26 @@
+'''
+Created on 2018年2月24日
+
+@author: BG246077
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testCreate(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testCreate']
+    unittest.main()
