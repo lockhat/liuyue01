6a8fb79d1ec7f4794a810009cc53d3581b1696aa
lockhat <1160444584@qq.com>
2018-02-25 13:37:41 +0800

    在客户端②增加module02模块

---
+++ package01/module03.py
@@ -0,0 +1,18 @@
+'''
+Created on 2018年2月25日
+
+@author: liuyue
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
